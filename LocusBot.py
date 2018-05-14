dc5fe4a4cb2b861498d6dc8657c5a141112ee9f1
Sam Premutico <samprem@stanford.edu>
2018-05-14 11:13:53 -0700

    added interactive

--- LocusBot.py
+++ LocusBot.py
@@ -14,13 +14,15 @@ access_token = "NMY3F7NH72IG6SKUC2KRHSBAIDRXMGKM"
 
 client = Wit(access_token=access_token)
 locus = Locus(LOCATION_DATA_PATH)
+query = raw_input("\n\n> Hi! I'm Locus, your virtual assistant for all things location.\n> Ask me things like \" Where was I last night?\" or \" Where was I Febraury 2nd at 4:15pm?\"\n> ")
+while query != "quit":
+	resp = client.message(query)
 
-resp = client.message('Where was I last night?')
-
-if 'intent' not in resp['entities']:
-	print ("Locus could not locate intent. Please change your wording.")
-else:
-	intent = resp['entities']['intent']
-	result = locus.processIntent(intent, resp)
-	print(result)
+	if 'intent' not in resp['entities']:
+		print ("\n\nLocus could not locate intent. Please change your wording.")
+	else:
+		intent = resp['entities']['intent']
+		result = locus.processIntent(intent, resp)
+		print(result)
+	query = raw_input("\n\n> What else can I tell you?\n> ")
 #client.interactive()
