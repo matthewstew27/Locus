baaed18b1017f4da856bd526f54e53ceb2fe4c61
Matthew <matthewstewart@DN0a1f4c9f.SUNet>
2018-06-09 19:18:52 -0700

    added cors

--- LocusServer.py
+++ LocusServer.py
@@ -6,6 +6,8 @@ from wit import Wit
 from Locus import Locus
 import geopy
 import googlemaps
+from flask import Flask
+from flask_cors import CORS
 
 LOCATION_DATA_PATH = "data/LocationHistory.json"
 access_token = "NMY3F7NH72IG6SKUC2KRHSBAIDRXMGKM"
@@ -14,6 +16,7 @@ client = Wit(access_token=access_token)
 locus = Locus(LOCATION_DATA_PATH)
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def hello():
@@ -26,6 +29,7 @@ def init():
 
 @app.route('/chat/<string:query>/')
 def chat(query):
+	print(query)
 	result = 'Locus could not locate intent. Please change your wording.'
 	resp = client.message(query)
 	if 'intent' not in resp['entities']:
