cf699f3b6846d444631073db3d6ad932db9cfaa8
Matthew <matthewstewart@DN0a1f4c9f.SUNet>
2018-06-11 01:15:22 -0700

    dealing with async

--- CS294/components/chat/chatController.js
+++ CS294/components/chat/chatController.js
@@ -8,26 +8,46 @@
 cs142App.controller('ChatController', ['$scope', '$routeParams', '$resource', '$mdDialog',
 	function ($scope, $routeParams, $resource, $mdDialog) {
 		var userId = $routeParams.userId;
-		$scope.response='';
+		$scope.response='placeholder';
     $scope.input = '';
 		$scope.main.label = "Favorites View";
 	    var localResource = $resource("http://localhost:3000/favoriteSecond");
 	    localResource.query({}, function(favs) {
 	    	$scope.userFavs = favs;
 	    });
+          $scope.$watch("response", {
+      function() {
+        console.log("response updated");
+      }
+    });
 	$.ajaxSetup({
-		async: false
+		async: true
 	});
+
 	//$scope.getResponse = function() {
-    $.getJSON('http://127.0.0.1:5000/chat/' + "Where was I on tuesday?",
+    /*$.getJSON('http://127.0.0.1:5000/chat/' + "Where was I on tuesday?",
     function(data, textStatus, jqXHR) {
   		$scope.response = data;
   		console.log("DATA: " + JSON.stringify(data));
   		//console.log("DATA: " + data);
-    });
+    });*/
+
+    $scope.getResponse = function() {
+      $.ajax('http://127.0.0.1:5000/chat/' + $scope.input, {
+        dataType: "JSON",
+        success: function (response, statusTExt, XHR) {
+          console.log("IN CALLBACK FUNCTION: " + response);
+          $scope.response = response;
+        },
+        error: function(response, statusText, XHR) {
+          console.log("ERROR!! " + JSON.stringify(response) + JSON.stringify(statusText));
+        }
+      });
+    }
+
 
   //};
-  console.log("response: " + $scope.response);
+  //console.log("response: " + $scope.response);
 }]);
 
 
